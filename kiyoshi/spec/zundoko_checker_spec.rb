084384701f87943c9f4e39bd3ee9f0c99b47b914
hironoei.tanoue <hironoei.tanoue@E-1609-000086-03.local>
2018-09-05 15:15:57 +0900

    spec modified

--- kiyoshi/spec/zundoko_checker_spec.rb
+++ kiyoshi/spec/zundoko_checker_spec.rb
@@ -5,18 +5,45 @@ describe ZundokoChecker do
   it { expect(ZundokoChecker.new([])).not_to eq nil }
 
   describe '#read' do
+    let(:instance) { described_class.new(['']) }
+    subject { instance.read(params) }
+
     context '引数がnilの場合' do
+      let(:params) {}
+      it { is_expected.not_to eq nil }
     end
+
     context '引数が空の場合' do
+      let(:params) { '' }
+      it { is_expected.not_to eq nil }
     end
+
     context '引数の文字列がある場合' do
+      let(:params) { 'ずん' }
+      it { is_expected.not_to eq nil }
     end
   end
 
   describe '#next_kiyoshi?' do
-    context '想定の配列と一致する場合' do
+    let(:instance) { described_class.new(['ずん']) }
+    subject { instance.next_kiyoshi? }
+
+    context '初回呼び出し' do
+      it { is_expected.to eq false }
     end
+
+    context '想定の配列と一致する場合' do
+      before do
+        instance.read('ずん')
+      end
+      it { is_expected.to eq true }
+    end
+
     context '想定の配列と一致しない場合' do
+      before do
+        instance.read('どこ')
+      end
+      it { is_expected.to eq false }
     end
   end
 end
