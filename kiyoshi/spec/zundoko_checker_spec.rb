3a3c29075e60815426c2d7ca61671d0d471fc2be
hironoei.tanoue <hironoei.tanoue@E-1609-000086-03.local>
2018-09-05 10:03:42 +0900

    spec modified

--- kiyoshi/spec/zundoko_checker_spec.rb
+++ kiyoshi/spec/zundoko_checker_spec.rb
@@ -3,4 +3,20 @@ require "./app/zundoko_checker"
 
 describe ZundokoChecker do
   it { expect(ZundokoChecker.new([])).not_to eq nil }
+
+  describe '#read' do
+    context '引数がnilの場合' do
+    end
+    context '引数が空の場合' do
+    end
+    context '引数の文字列がある場合' do
+    end
+  end
+
+  describe '#next_kiyoshi?' do
+    context '想定の配列と一致する場合' do
+    end
+    context '想定の配列と一致しない場合' do
+    end
+  end
 end
